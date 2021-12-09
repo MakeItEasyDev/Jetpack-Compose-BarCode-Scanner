ddc1e47d611d96ecc1e81adcd13a0569d59ed5b0
MakeItEasyDev <89842411+MakeItEasyDev@users.noreply.github.com>
2021-12-09 22:40:29 +0530

    Add files via upload

---
+++ app/src/main/java/com/jetpack/barcodescanner/MainActivity.kt
@@ -0,0 +1,154 @@
+package com.jetpack.barcodescanner
+
+import android.Manifest
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.common.util.concurrent.ListenableFuture
+import com.jetpack.barcodescanner.ui.theme.BarcodeScannerTheme
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+@ExperimentalPermissionsApi
+class MainActivity : ComponentActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BarcodeScannerTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Spacer(modifier = Modifier.height(10.dp))
+                        
+                        val cameraPermissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+                        
+                        Button(
+                            onClick = {
+                                cameraPermissionState.launchPermissionRequest()
+                            }
+                        ) {
+                            Text(text = "Camera Permission")
+                        }
+
+                        Spacer(modifier = Modifier.height(10.dp))
+                        
+                        CameraPreview()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CameraPreview() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    var preview by remember { mutableStateOf<Preview?>(null) }
+    val barCodeVal = remember { mutableStateOf("") }
+    
+    AndroidView(
+        factory = { AndroidViewContext ->
+            PreviewView(AndroidViewContext).apply { 
+                this.scaleType = PreviewView.ScaleType.FILL_CENTER
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                )
+                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+            }
+        },
+        modifier = Modifier
+            .fillMaxSize(),
+        update = { previewView ->
+            val cameraSelector: CameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+            val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+            val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> = 
+                ProcessCameraProvider.getInstance(context)
+            
+            cameraProviderFuture.addListener({
+                preview = Preview.Builder().build().also { 
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+                val barcodeAnalyser = BarCodeAnalyser { barcodes ->
+                    barcodes.forEach { barcode ->
+                        barcode.rawValue?.let { barcodeValue ->
+                            barCodeVal.value = barcodeValue
+                            Toast.makeText(context, barcodeValue, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                    }
+
+                try {
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        imageAnalysis
+                    )
+                } catch (e: Exception) {
+                    Log.d("TAG", "CameraPreview: ${e.localizedMessage}")
+                }
+            }, ContextCompat.getMainExecutor(context))
+        }
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
